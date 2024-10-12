2666277b8e0be8346fc77c2ffdd4b0ea77392444
Claudia Devis <claudiagdevis@gmail.com>
2024-10-12 08:21:47 +0200

    Creación del menú (diccionario)

---
+++ menu.py
@@ -0,0 +1,15 @@
+menu = {
+    "Aperitivo: Guacamole": ["cilantro", "cebolla","chile","ajo","tomate","aguacate","zumo de limón","sal"],
+    "Primer plato: Pimientos del piquillo rellenos" : ["huevo","palitos de cangrejo","atún en acete de oliva","mayonesa","pimientos del piquillo","pimiento verde","pimiento rojo","cebolla","vinagre de vino","sal","aceite de oliva"],
+    "Segundo plato: Solomillo de cerdo agridulce" : ["aceite de oliva","ajo","solomillo de cerdo","cebolla","manzana","tomate concentrado","calabacines","brandy","vino de Oporto","ciruelas pasas sin hueso","orejones de albaricoque","mostaza","piñones tostados"],
+    "Postre: Brownie de chocolate con helado de turrón": ["chocolate amargo","mantequilla sin sal","azúcar","huevos","sal","harina de trigo","nueces"],
+}
+
+def lista_de_compras(menu):
+    """
+    Devuelve la lista de compras de un menú que es un diccionario
+    """
+    
+    return
+
+
